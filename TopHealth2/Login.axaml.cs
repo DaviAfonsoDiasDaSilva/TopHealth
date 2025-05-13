22c7533c0939a650d714b4cb33bc787f7a895f8a
DaviAfonsoDiasDaSilva <daviafonsodiasdasilva@gmail.com>
2025-05-13 19:35:54 -0300

    Tela de Login Parte Grafica(Finalizada)
    Falta fazer a parte funcional

--- TopHealth2/Login.axaml.cs
+++ TopHealth2/Login.axaml.cs
@@ -14,4 +14,10 @@ public partial class Login : Window
         HistRegiWin.Show();
         this.Close();
     }
+    private void BotRet(object? sender, RoutedEventArgs e)
+    {
+        var TelaIni = new MainWindow();
+        TelaIni.Show();
+        this.Close();
+    }
 }
